7d316c60aeaa2f7e57d56f148162ef9981f4d1f1
atidele <alejandro.tidele@mercadolibre.com>
2020-06-06 14:20:52 -0300

    Reflection pattern is added 🚀

--- src/main/java/UTN/FRGP/UTN_LABV_TP_Integrador/App.java
+++ src/main/java/UTN/FRGP/UTN_LABV_TP_Integrador/App.java
@@ -1,18 +1,9 @@
 package UTN.FRGP.UTN_LABV_TP_Integrador;
 
-import UTN.FRGP.UTN_LABV_TP_Integrador.Seeders.UserSeeder;
-import UTN.FRGP.UTN_LABV_TP_Integrador.Seeders.LocationSeeder;
-import UTN.FRGP.UTN_LABV_TP_Integrador.Services.SessionService;
-import org.hibernate.Session;
+import UTN.FRGP.UTN_LABV_TP_Integrador.Seeders.Seeder;
 
 public class App {
     public static void main( String[] args ) {
-        Session session = SessionService.getSession();
-
-        LocationSeeder.hydrate(session);
-        UserSeeder.hydrate(session);
-
-        SessionService.commitSession(session);
-        SessionService.closeSessionFactory();
+        Seeder.initialize();
     }
 }
