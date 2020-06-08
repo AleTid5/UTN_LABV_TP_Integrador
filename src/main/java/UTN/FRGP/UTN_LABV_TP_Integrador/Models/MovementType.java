10bd295d2b2f83b59d89c6baf44581a08fe5ddfe
Dangelo Martin <martinalexisdangelo@gmail.com>
2020-06-07 22:15:44 -0300

    Se agrega MovementSeeder

--- src/main/java/UTN/FRGP/UTN_LABV_TP_Integrador/Models/MovementType.java
+++ src/main/java/UTN/FRGP/UTN_LABV_TP_Integrador/Models/MovementType.java
@@ -21,4 +21,26 @@ public class MovementType {
     @OneToOne
     @JoinColumn
     private CurrencyType currencyType;
+
+    public MovementType(String name, CurrencyType currencyType)
+    {
+        this.setName(name);
+        this.setCurrencyType(currencyType);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CurrencyType getCurrencyType() {
+        return currencyType;
+    }
+
+    public void setCurrencyType(CurrencyType currencyType) {
+        this.currencyType = currencyType;
+    }
 }
