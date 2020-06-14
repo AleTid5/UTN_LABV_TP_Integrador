8e435284f67217f31a022dbad81ca43b486c7589
atidele <alejandro.tidele@mercadolibre.com>
2020-06-14 13:51:59 -0300

    Starting with Spring MVC 🚀

---
+++ src/main/java/UTN/FRGP/TP_L5_GRUPO_1/Controllers/LoginController.java
@@ -0,0 +1,18 @@
+package UTN.FRGP.TP_L5_GRUPO_1.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("")
+@Controller
+public class LoginController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap modelMap) {
+        modelMap.addAttribute("message", "Hi!");
+
+        return "hello";
+    }
+}
