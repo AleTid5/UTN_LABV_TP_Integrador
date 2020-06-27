7240eecd6fb5ffd7dd1a720bd3d20f01cedcddf6
atidele <alejandro.tidele@mercadolibre.com>
2020-06-26 22:25:22 -0300

    Rename of main.

--- src/main/java/UTN/FRGP/TP_L5_GRUPO_1/Main.java
+++ src/main/java/UTN/FRGP/TP_L5_GRUPO_1/Main.java
@@ -2,7 +2,7 @@ package UTN.FRGP.TP_L5_GRUPO_1;
 
 import UTN.FRGP.TP_L5_GRUPO_1.Seeders.Seeder;
 
-public class App {
+public class Main {
     public static void main( String[] args ) {
         Seeder.plant();
     }
